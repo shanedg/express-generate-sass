dd609d30cb3e81d2d7d5f5b88f6713d4eeb3c1c8
Shane Garrity <shanedgarrity@gmail.com>
2019-02-02 02:46:14 -0500

    needed absolute path to pwd for watch/restart events

--- forever.js
+++ forever.js
@@ -1,6 +1,8 @@
 var forever = require('forever-monitor');
+var pwd = process.cwd();
 
 var child = new (forever.Monitor)('./bin/www', {
+  'cwd': pwd,
   'killTree': true,
   'logFile': 'logs/log',
   'outFile': 'logs/out',
@@ -8,13 +10,26 @@ var child = new (forever.Monitor)('./bin/www', {
   'minUptime': 500,
   'spinSleepTime': 1000,
   'silent': false,
+  'sourceDir': pwd,
   'watch': true,
-  'watchDirectory': '.',
+  'watchDirectory': pwd,
   'watchIgnoreDotFiles': true,
+  'watchIgnorePatterns': [
+    'forever.js',
+  ],
+
 });
 
 child.on('start', function() {
   console.log('forever start');
+});
+
+child.on('restart', function() {
+  console.log(`forever restart #${child.times}`);
+});
+
+child.on('watch:restart', function(info) {
+  console.log('forever watch:restart', info.stat, info.file);
 });
 
 child.start();
